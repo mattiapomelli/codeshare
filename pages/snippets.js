87386f47bc36e0061b917773b2cc674600ce17db
mattiapomelli <mattiapomelli@gmail.com>
2020-11-10 23:13:27 +0100

    useSwr infinite for loading more

--- pages/snippets.js
+++ pages/snippets.js
@@ -1,25 +1,40 @@
-import { useState, useRef, useEffect } from 'react'
-import { useQuery } from '@apollo/client'
-import { GET_FILTERED_SNIPPETS_QUERY } from "../graphql/queries"
+import {  useEffect } from 'react'
+import { request } from "graphql-request"
+import { useSWRInfinite } from 'swr'
 import scrolledToBottom from "../utils/scrolled-to-bottom"
 import SnippetCard from "../components/SnippetCard"
 import { useSearch } from '../contexts/SearchContext'
 import { Grid, Tabs, SearchBar, Spinner } from "../components/elements/HomeElements"
 
+const GET_FILTERED_SNIPPETS_QUERY = `
+	query SearchSnippetsQuery($search: String, $programmingLang: String, $order: order_by, $limit: Int!, $offset: Int!) {
+	snippets: search_snippet(args: {search: $search}, where: {programmingLang: {_eq: $programmingLang}}, order_by: {createdAt: $order}, limit: $limit, offset: $offset) {
+		title
+		code
+		createdAt
+		programmingLang
+		id
+	}
+	}
+`
+const variables = {
+	limit: 6,
+	order: "desc",
+	programmingLang: null,
+	search: ''
+}
+const fetcher = (query, offset) => request('https://climbing-bear-85.hasura.app/v1/graphql', query, {...variables, offset})
+
 export default function Home() {
-	const offset = useRef(0)
-	const hasMoreData = useRef(true)
-	const [moreData, setMoreData ] = useState(true);
 	const { search, setSearch, activeLanguage, setActiveLanguage } = useSearch();
-	const { loading, data, fetchMore } = useQuery(GET_FILTERED_SNIPPETS_QUERY, {
-		variables: {
-			offset: offset.current,
-			limit: 6,
-			order: search > 0 ? null : "desc",
-			programmingLang: activeLanguage,
-			search: search
-		}
-	})
+	const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
+		index => [GET_FILTERED_SNIPPETS_QUERY, index*6],
+		fetcher
+	)
+
+	const isLoadingInitialData = !data && !error;
+	const isLoadingMore = isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === "undefined");
+	console.log('Loading more: ', isLoadingMore)
 
 	const languages = ["Java", "JavaScript", "CSS", "HTML", "SQL", "C"]
 
@@ -29,26 +44,10 @@ export default function Home() {
 		return () => { window.removeEventListener('scroll', handleScroll)}
 	}, [])
 
-	let fetching = false;
 	const handleScroll = () => {
-		if ( scrolledToBottom() && hasMoreData.current && !fetching) {
-			fetching = true;
-			offset.current += 6
-			fetchMore({
-				variables: {
-					offset: offset.current,
-				}
-			}).then(result => {
-				fetching = false
-				if(result.data.snippets.length == 0)
-					setMoreData(false)
-			})
+		if ( scrolledToBottom()) {
 		}
 	}
-
-	useEffect(() => {
-		hasMoreData.current = moreData
-	}, [moreData])
 
 	return (
 		<div>
@@ -64,12 +63,16 @@ export default function Home() {
 				))}
 			</Tabs>
 			<Grid>
-				{!loading &&
-					data.snippets.map((snippet, index) => (
-						<SnippetCard {...snippet} key={index} preview={true} />
-					))}
+				{ !isLoadingInitialData && 
+					data.map(set => {
+						return set.snippets.map((snippet, index) => (
+							<SnippetCard {...snippet} key={index} preview={true} />
+						))
+					})
+				}
+					
 			</Grid>
-			{ moreData && <Spinner />}
+			<button onClick={() => setSize(size + 1)}>Load more</button>
 		</div>
 	)
 }
