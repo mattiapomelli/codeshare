bfd96329798f77852864f0e77005b0255662ed1b
mattiapomelli <mattiapomelli@gmail.com>
2020-12-20 17:38:54 +0100

    email validation

--- utils/registerValidation.js
+++ utils/registerValidation.js
@@ -1,3 +1,5 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 function validateRegisterInput(data) {
 
     const { username, email, password } = data
@@ -6,6 +8,7 @@ function validateRegisterInput(data) {
     if(!username) return new Error("Username is required")
     if(!password) return new Error("Password is required")
 
+    if(!email.match(emailRegex)) return new Error("Email must be a valid email")
     if(username.length < 4 || username.length > 10) return new Error("Username must be between 4 and 10 characters long")
     if(password.length < 6) return new Error("Password must be at least 6 characters long")
 
