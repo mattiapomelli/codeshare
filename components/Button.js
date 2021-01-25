da1bfc437c7e1236a2f818b1f4715b14fdae7b05
mattiapomelli <mattiapomelli@gmail.com>
2021-01-25 18:11:55 +0100

    prettier config

--- components/Button.js
+++ components/Button.js
@@ -1,99 +1,106 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 import { Icon } from './Icon/Icon'
 
 export const Button = styled.button`
-    border: none;
-    outline: none;
-    font-family: inherit;
-    font-size: 0.8rem;
-    font-weight: 500;
-    padding: ${props => props.padding ? props.padding : props.small ? "1.1em 2.6em" : "1.4em 2.6em"};
-    border-radius: ${props => props.borderRadius || "10em"};
-    cursor: pointer;
-    min-width: ${props => props.minWidth || 'auto'};
-    text-align: center;
+  border: none;
+  outline: none;
+  font-family: inherit;
+  font-size: 0.8rem;
+  font-weight: 500;
+  padding: ${(props) =>
+    props.padding
+      ? props.padding
+      : props.small
+      ? '1.1em 2.6em'
+      : '1.4em 2.6em'};
+  border-radius: ${(props) => props.borderRadius || '10em'};
+  cursor: pointer;
+  min-width: ${(props) => props.minWidth || 'auto'};
+  text-align: center;
 
-    background: ${props => {
-        switch(props.type) {
-            case "primary":
-                return props.theme.colors.primary
-            case "inverted":
-                return props.theme.colors.text
-            case "secondary":
-                return props.theme.colors.accent
-            default:
-                return props.theme.colors.elements
-        }
-    }};
-
-    color: ${props => {
-        switch(props.type) {
-            case "primary":
-                return "white"
-            case "inverted":
-                return props.theme.colors.background
-            default:
-                return props.theme.colors.text
-        }
-    }};
-
-    &:hover:not(:disabled) {
-        background: ${props => {
-            switch(props.type) {
-                case "primary":
-                    return props.theme.colors.primaryHover
-                case "inverted":
-                    return "white"
-                case "secondary":
-                    return props.theme.colors.details
-                default:
-                    return props.theme.colors.accent
-            }
-        }};
+  background: ${(props) => {
+    switch (props.type) {
+      case 'primary':
+        return props.theme.colors.primary
+      case 'inverted':
+        return props.theme.colors.text
+      case 'secondary':
+        return props.theme.colors.accent
+      default:
+        return props.theme.colors.elements
     }
+  }};
 
-    &:disabled {
-        cursor: not-allowed;
-        opacity: 60%;
+  color: ${(props) => {
+    switch (props.type) {
+      case 'primary':
+        return 'white'
+      case 'inverted':
+        return props.theme.colors.background
+      default:
+        return props.theme.colors.text
     }
+  }};
+
+  &:hover:not(:disabled) {
+    background: ${(props) => {
+      switch (props.type) {
+        case 'primary':
+          return props.theme.colors.primaryHover
+        case 'inverted':
+          return 'white'
+        case 'secondary':
+          return props.theme.colors.details
+        default:
+          return props.theme.colors.accent
+      }
+    }};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 60%;
+  }
 `
 
 const StyledFlexButton = styled(Button)`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    svg { margin-left: 5px; };
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    margin-left: 5px;
+  }
 `
 
-const StyledIconButton = styled(Button).attrs(props => ({
-    padding: props.small ? "0.5em" : "0.9em",
-    borderRadius: "1.2em",
+const StyledIconButton = styled(Button).attrs((props) => ({
+  padding: props.small ? '0.5em' : '0.9em',
+  borderRadius: '1.2em',
 }))`
-    line-height: 1;
+  line-height: 1;
 `
 
 export const IconButton = ({ icon, iconType, ...rest }) => (
-    <StyledIconButton {...rest}>
-        <Icon name={icon} type={iconType}/>
-    </StyledIconButton>
+  <StyledIconButton {...rest}>
+    <Icon name={icon} type={iconType} />
+  </StyledIconButton>
 )
 
-export const FlexButton= ({ children, icon, iconType, small, ...rest }) => {
-    return (
-        <StyledFlexButton {...rest} small={small}>
-            {children}
-            <Icon name={icon} size={small ? 12 : 20} type={iconType}/>
-        </StyledFlexButton>
-    )
+export const FlexButton = ({ children, icon, iconType, small, ...rest }) => {
+  return (
+    <StyledFlexButton {...rest} small={small}>
+      {children}
+      <Icon name={icon} size={small ? 12 : 20} type={iconType} />
+    </StyledFlexButton>
+  )
 }
 
 export const Tab = styled.div`
-    font-size: 0.8rem;
-    font-weight: 500;
-    padding: 1.1em 2.6em;
-    border-radius: 10em;
-    min-width: ${props => props.minWidth || 'auto'};
-    text-align: center;
-    background-color: ${props => props.theme.colors.elements};
-    color: ${props => props.theme.colors.text};
+  font-size: 0.8rem;
+  font-weight: 500;
+  padding: 1.1em 2.6em;
+  border-radius: 10em;
+  min-width: ${(props) => props.minWidth || 'auto'};
+  text-align: center;
+  background-color: ${(props) => props.theme.colors.elements};
+  color: ${(props) => props.theme.colors.text};
 `
--- components/Icon/Icon.js
+++ components/Icon/Icon.js
@@ -1,31 +1,27 @@
 import * as icons from './icons'
-import styled from "styled-components"
+import styled from 'styled-components'
 
 const SVG = styled.svg`
-    fill: ${props => {
-        switch(props.type) {
-            case "primary":
-                return "white"
-            case "inverted":
-                return props.theme.colors.background
-            default:
-                return props.theme.colors.details
-        }
-    }};
-    width: ${props => (props.size ? `${props.size/16}rem` : '1.5rem')};
-    height: ${props => (props.size ? `${props.size/16}rem` : '1.5rem')};
+  fill: ${(props) => {
+    switch (props.type) {
+      case 'primary':
+        return 'white'
+      case 'inverted':
+        return props.theme.colors.background
+      default:
+        return props.theme.colors.details
+    }
+  }};
+  width: ${(props) => (props.size ? `${props.size / 16}rem` : '1.5rem')};
+  height: ${(props) => (props.size ? `${props.size / 16}rem` : '1.5rem')};
 `
 
 export const Icon = ({ name, ...rest }) => {
-    const Path = icons[name]
+  const Path = icons[name]
 
-    return (
-        <SVG
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            {...rest}
-        >
-            <Path/>
-        </SVG>
-    )
+  return (
+    <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...rest}>
+      <Path />
+    </SVG>
+  )
 }
